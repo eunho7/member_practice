134ad2e72f5e4f8d6f93aa76bd3df03764f3a87c
Eunho <76768140sa@gmail.com>
2024-10-08 16:52:32 +0900

    first commit

---
+++ src/main/java/com/exapmle/member/MemberPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.exapmle.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exapmle/member/MemberPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exapmle.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
