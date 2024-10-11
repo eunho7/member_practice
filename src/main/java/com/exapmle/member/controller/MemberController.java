82144f69291e063aa1bbe560ab290a4d775611e7
Eunho <76768140sa@gmail.com>
2024-10-11 14:46:41 +0900

    feat 회원가입 구현

--- src/main/java/com/exapmle/member/controller/MemberController.java
+++ src/main/java/com/exapmle/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.exapmle.member.controller;
 
 import com.exapmle.member.dto.MemberDTO;
 import com.exapmle.member.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,5 +25,18 @@ public class MemberController {
     public String save(@ModelAttribute MemberDTO memberDTO) {
         memberService.save(memberDTO);
         return "login";
+    }
+
+    @PostMapping("/member/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+        MemberDTO loginResult = memberService.login(memberDTO);
+        if(loginResult != null){
+            // login 성공
+            session.setAttribute("loginEmail", loginResult.getMemberEmail());
+            return "main";
+        } else {
+            return "redirect:/member/login";
+        }
+
     }
 }
--- src/main/java/com/exapmle/member/service/MemberService.java
+++ src/main/java/com/exapmle/member/service/MemberService.java
@@ -6,6 +6,9 @@ import com.exapmle.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Member;
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class MemberService {
@@ -13,5 +16,30 @@ public class MemberService {
 
     public void save(MemberDTO memberDTO) {
         memberRepository.save(MemberEntity.toSaveEntity(memberDTO));
+    }
+
+    public MemberDTO login(MemberDTO memberDTO) {
+        /*
+            1. 회원이 입력한 이메일로 DB에서 조회함.
+            2. DB에서 조회한 비밀번호와 사용자가 입력한 비밀번호가 일치하는지 판단
+        */
+        Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberDTO.getMemberEmail());
+        if(byMemberEmail.isPresent()){
+            // 조회 결과가 있다.
+            // 해당 이메일을 가진 회원 정보가 있다.
+            MemberEntity memberEntity = byMemberEmail.get();
+            if(memberEntity.getMemberPassword().equals(memberDTO.getMemberPassword())){
+                // 비밀번호 일치
+                // entity -> dto 변환 후 리턴
+                return MemberDTO.toMemberDTO(memberEntity);
+            }else{
+                // 비밀번호 불 일치
+                return null;
+            }
+        } else {
+            // 조회 결과가 없다.
+            // 해당 이메일이 없다.
+            return null;
+        }
     }
 }
